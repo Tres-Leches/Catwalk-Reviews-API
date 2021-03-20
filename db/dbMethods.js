441cc75ea19284274fca853fa705800069137d8f
Chris Wang <heyitschrisw@gmail.com>
2021-03-20 01:16:11 -0700

    Added server routes to POST

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -2,29 +2,54 @@ const db = require('./index.js');
 
 const dbMethods = {
   getAllReviews: (params, callback) => {
+    let sort, page;
+    switch (params.sort) {
+      case 'newest':
+        sort = 'date';
+        break;
+      case 'helpful':
+      case 'relevant':
+        sort = 'helpfulness';
+        break;
+      default:
+        sort = 'review_id';
+        break;
+    }
+    switch(true) {
+      case (params.page > 1):
+        page = params.page - 1;
+        break;
+      case (params.page <= 1):
+        page = 0;
+        break;
+    }
     let object = {
       product: params.product_id,
-      page: params.page || 0,
+      page: page || 0,
       count: params.count || 5,
     };
+
     const query = `
-      SELECT r.id AS review_id,
-        r.rating,
-        r.summary,
-        r.recommend,
-        r.response,
-        r.body,
-        r.date,
-        r.reviewer_name,
-        r.helpfulness,
-        json_agg(
-          json_build_object('photo_id', p.id, 'url', p.url)) as photos
-      FROM reviews r
-      LEFT JOIN reviews_photos p
-      ON r.id = p.review_id
-      WHERE r.product_id = ${params.product_id}
-      GROUP BY r.id
-      ORDER BY r.id;
+      WITH response AS (
+        SELECT r.id AS review_id,
+          r.rating,
+          r.summary,
+          r.recommend,
+          r.response,
+          r.body,
+          r.date,
+          r.reviewer_name,
+          r.helpfulness,
+          COALESCE(json_agg(
+            json_build_object('photo_id', p.id, 'url', p.url)) FILTER (WHERE p.id IS NOT NULL), '[]') as photos
+        FROM reviews r
+        LEFT JOIN reviews_photos p ON r.id = p.review_id
+        WHERE r.product_id = ${object.product}
+        GROUP BY r.id
+        OFFSET ${object.page * object.count} ROWS
+        FETCH FIRST ${object.count} ROWS ONLY
+      )
+      SELECT * FROM response ORDER BY ${sort} DESC
     `;
     db.query(query)
       .then((res) => {
@@ -39,7 +64,7 @@ const dbMethods = {
     };
     const ratingsQuery = `
       WITH ratings AS (
-        SELECT rating,COUNT(rating)
+        SELECT rating, COUNT(rating)
         FROM reviews
         WHERE product_id = ${params.product_id}
         GROUP BY rating
@@ -74,7 +99,6 @@ const dbMethods = {
         GROUP BY c.name, cr.characteristic_id
         ORDER BY cr.characteristic_id ASC
         )
-
       SELECT
         json_object_agg(name, json_build_object('id', characteristic_id, 'value', value))
         AS characteristics
@@ -100,19 +124,45 @@ const dbMethods = {
     .catch((err) => callback(err));
   },
 
-  postReviewById: (callback) => {
-    callback(null, 'postReviewById')
+  postReviewById: (req_body, callback) => {
+    const { product_id, rating, summary, body, recommend, name, email, photos, characteristics } = req_body;
+    const insReviewsQuery = `
+      INSERT INTO reviews(product_id, rating, summary, body, recommend, reviewer_name, reviewer_email)
+      VALUES (${product_id}, ${rating}, '${summary}', '${body}', ${recommend}, '${name}', '${email}')
+    `;
+    console.log('photos', photos);
+    if (photos.length > 0) {
+      var insPhotosQuery = `INSERT INTO reviews_photos(review_id, url) VALUES`;
+      for (let i = 0; i < photos.length; i++) {
+        if (i === photos.length - 1) {
+          insPhotosQuery += ` \ (currval(pg_get_serial_sequence('reviews', 'id')), '${photos[i]}')`;
+        } else {
+          insPhotosQuery += ` \ (currval(pg_get_serial_sequence('reviews', 'id')), '${photos[i]}'),`;
+        }
+      }
+    } else {
+      var insPhotosQuery = null;
+    }
 
+    console.log(insPhotosQuery);
+    db.query(insReviewsQuery)
+      .then(() => {
+        console.log('l146', insPhotosQuery);
+        if (insPhotosQuery) {
+          console.log('l148', insPhotosQuery)
+          db.query(insPhotosQuery);
+        }
+      })
+      .then(() => callback(null, 'CREATED'))
+      .catch((err) => console.log('err',err));
   },
 
   markReviewAsHelpful: (callback) => {
     callback(null, 'markReviewAsHelpful')
-
   },
 
   reportReview: (callback) => {
     callback(null, 'reportReview')
-
   },
 }
 
--- server/controller.js
+++ server/controller.js
@@ -3,22 +3,29 @@ const dbMethods = require('../db/dbMethods.js');
 const controller = {
   getAllReviews: (req, res) => {
     const params = req.query;
-    dbMethods.getAllReviews(params, (err, results) => {
-      err ? res.status(400).send(err) : res.status(200).send(results)
-    })
+    !params.product_id ?
+      res.status(422).send('Error: Invalid product_id provided') :
+      dbMethods.getAllReviews(params, (err, results) => {
+        err ? res.status(400).send(err) : res.status(200).send(results);
+      });
   },
 
   getMetadataById: (req, res) => {
     const params = req.query;
-    dbMethods.getMetadataById(params, (err, results) => {
-      err ? res.status(400).send(err) : res.status(200).send(results)
-    })
+    !params.product_id ?
+      res.status(422).send('Error: Invalid product_id provided') :
+      dbMethods.getMetadataById(params, (err, results) => {
+        err ? res.status(400).send(err) : res.status(200).send(results)
+      })
   },
 
   postReviewById: (req, res) => {
-    dbMethods.postReviewById((err, results) => {
-      err ? res.status(400).send(err) : res.status(200).send(results)
-    })
+    const body = req.body;
+    !body ?
+      res.status(422).send('Error: Invalid product_id provided') :
+      dbMethods.postReviewById(body, (err, results) => {
+        err ? res.status(400).send(err) : res.status(200).send(results)
+      })
   },
 
   markReviewAsHelpful: (req, res) => {
